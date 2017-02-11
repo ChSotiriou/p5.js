e26828ee00457f1f3ebb1e87211ff18fe66d4546
Christos_Soteriou <schris15@hotmail.com>
2017-02-11 13:27:33 +0200

    UPDATE

--- FractalTreePerlinNoise/sketch.js
+++ FractalTreePerlinNoise/sketch.js
@@ -3,32 +3,45 @@ var xoff;
 var st = 0;
 var inc = 0.01;
 var stInc = 0.0005;
+var cT = 0;
 
 var speed;
 var hidden = false;
+var p;
+
+var canvas;
 
 function setup() {
-  createCanvas(1000, 800);
+  canvas = createCanvas(1000, 800);
+  canvas.position(displayWidth / 2 - width / 2, displayHeight / 2 - height / 2);
   colorMode(HSB);
 
-  speed = createSlider(0, 0.1, 0, 0.0001);
+  speed = createSlider(0, 180, 0, 1);
   speed.position(10, 10);
 
+  p = createP();
+  p.position(10, 20);
+
   a = 0;
+  make();
 }
 
 function draw() {
+  if (millis() - cT > 500) {
+    cT = millis();
+    if (a != speed.value()) make();
+  }
+}
+
+function make() {
   background(20);
   strokeWeight(4);
   translate(width / 2, height);
 
-  stInc = speed.value();
-
-  xoff = st;
+  a = speed.value();
+  p.html(a);
 
   branch(height / 2.96);
-
-  st += stInc;
 }
 
 function branch(len) {
@@ -36,26 +49,29 @@ function branch(len) {
   line(0, 0, 0, -len);
   translate(0, -len);
   if (len > 4) {
-    a = noise(xoff) * PI / 2;
     push();
-    rotate(radians(60));
+    rotate(radians(a));
     branch(len * 0.67);
     pop();
 
     push();
-    rotate(radians(-60));
+    rotate(radians(-a));
     branch(len * 0.67);
     pop();
-    xoff += inc;
   }
 }
 
 function keyPressed() {
-  if (hidden) {
-    hidden = false;
-    speed.show();
-  } else {
-    hidden = true;
-    speed.hide();
+  if (key == ' ') make();
+  else {
+    if (hidden) {
+      hidden = false;
+      speed.show();
+      p.show();
+    } else {
+      hidden = true;
+      speed.hide();
+      p.hide();
+    }
   }
 }
