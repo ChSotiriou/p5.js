fe8eba099be99632d2ea732fedf5399d59e194ee
Christos_Soteriou <schris15@hotmail.com>
2017-02-11 22:43:53 +0200

    UPDATE

--- Tetris/sketch.js
+++ Tetris/sketch.js
@@ -5,6 +5,10 @@ const mpmStart = 1000;
 var score = 0;
 var pElement;
 
+var pGame;
+var pName;
+var pStart;
+
 var millisPerMove = mpmStart;
 
 var cols;
@@ -12,6 +16,8 @@ var rows;
 
 var lastBlock;
 var blocks = [];
+
+var start = false;
 
 function setup() {
   createCanvas(480, 680);
@@ -21,38 +27,56 @@ function setup() {
   pElement = createP('Score: ');
   pElement.style('font-size', 36);
   pElement.position(25 + width, -20);
+  pElement.hide();
+
+  pGame = createP('Tetris');
+  pGame.position(width / 2 - 50, 50);
+  pGame.style('font-size', 50);
+
+  pName = createP('by Christos Soteriou');
+  pName.position(width / 2 - 190, 150);
+  pName.style('font-size', 50);
+
+  pStart = createP('Press any key to start');
+  pStart.position(width / 2 - 200, 250);
+  pStart.style('font-size', 50);
 
   lastBlock = new Block();
   blocks.push(lastBlock);
 }
 
 function draw() {
-  background(51);
+  if (start) {
+    background(51);
 
-  score = blocks.length - 1;
-  pElement.html('Score: ' + score);
+    score = blocks.length - 1;
+    pElement.html('Score: ' + score);
 
-  for (var i = 0; i < cols * scl; i += scl) {
-    for (var j = 0; j < rows * scl; j += scl) {
-      noFill();
-      stroke(255, 50);
-      rect(i, j, scl, scl);
+    for (var i = 0; i < cols * scl; i += scl) {
+      for (var j = 0; j < rows * scl; j += scl) {
+        noFill();
+        stroke(255, 50);
+        rect(i, j, scl, scl);
+      }
     }
-  }
-  lastBlock = blocks[blocks.length - 1];
+    lastBlock = blocks[blocks.length - 1];
 
-  if (lastBlock.getLanded()) {
-    addBlock();
-  }
-
-  for (var i = 0; i < blocks.length; i++) {
-    blocks[i].render();
-    if (blocks[i].checkLose()) {
-      lose();
-      return;
+    if (lastBlock.getLanded()) {
+      addBlock();
     }
+
+    for (var i = 0; i < blocks.length; i++) {
+      blocks[i].render();
+      if (blocks[i].checkLose()) {
+        lose();
+        return;
+      }
+    }
+    lastBlock.update();
+  } else {
+    background(204);
+
   }
-  lastBlock.update();
 }
 
 function lose() {
@@ -67,6 +91,13 @@ function addBlock() {
 }
 
 function keyPressed() {
+  if (!start) {
+    pElement.show();
+    pGame.hide();
+    pName.hide();
+    pStart.hide();
+    start = true;
+  }
   if (key === 'q' || key === 'Q') {
     lastBlock.changeAngle(-1);
   } else if (key === 'e' || key === 'E') {
