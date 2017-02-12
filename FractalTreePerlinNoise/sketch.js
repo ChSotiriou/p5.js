2037f81c4b68529cea0b1de9544eb97826768367
Christos_Soteriou <schris15@hotmail.com>
2017-02-12 16:28:54 +0200

    UPDATE

--- FractalTreePerlinNoise/sketch.js
+++ FractalTreePerlinNoise/sketch.js
@@ -6,13 +6,9 @@ var stInc = 0.0005;
 var cT = 0;
 
 var input;
-var sliderR;
-var sliderG;
-var sliderB;
+var colorT;
 
-var sliderRB;
-var sliderGB;
-var sliderBB;
+var randomR;
 
 var hidden = false;
 
@@ -23,57 +19,30 @@ function setup() {
   canvas.position(displayWidth / 2 - width / 2, displayHeight / 2 - height / 2);
   colorMode(RGB);
 
-  input = createInput('60');
-  input.changed(make);
-  input.position(10, 10);
+  input = document.getElementById("angle");
+  input.value = '60';
+  input.onchange = make;
 
-  sliderR = createSlider(0, 255, 255, 1);
-  sliderR.changed(make);
-  sliderR.position(10, 40);
+  colorT = document.getElementById("colT");
+  colorT.onchange = make;
+  colorT.value = "#ffffff";
 
-  sliderG = createSlider(0, 255, 255, 1);
-  sliderG.changed(make);
-  sliderG.position(10, 70);
-
-  sliderB = createSlider(0, 255, 255, 1);
-  sliderB.changed(make);
-  sliderB.position(10, 100);
-
-  sliderRB = createSlider(0, 255, 255, 1);
-  sliderRB.changed(make);
-  sliderRB.position(10, 140);
-
-  sliderGB = createSlider(0, 255, 255, 1);
-  sliderGB.changed(make);
-  sliderGB.position(10, 170);
-
-  sliderBB = createSlider(0, 255, 255, 1);
-  sliderBB.changed(make);
-  sliderBB.position(10, 200);
+  colorB = document.getElementById("colB");
+  colorB.onchange = make;
+  colorB.value = "#333333";
 
   make();
 }
 
 function make() {
-  var rB = sliderRB.value();
-  var gB = sliderGB.value();
-  var bB = sliderBB.value();
-
-  var col = color(rB, gB, bB);
-  var hx = "#" + hex(rB, 2) + hex(gB, 2) + hex(bB, 2);
-  document.body.style.backgroundColor = hx;
-  background(col);
-
-  var r = sliderR.value();
-  var g = sliderG.value();
-  var b = sliderB.value();
-
-  stroke(r, g, b);
+  background(colorB.value);
+  document.body.style.backgroundColor = colorB.value;
+  stroke(colorT.value);
 
   strokeWeight(4);
   translate(width / 2, height);
 
-  a = input.value();
+  a = input.value;
 
   branch(height / 2.96);
 }
@@ -98,26 +67,15 @@ function keyPressed() {
   if (key == 'h' || key == 'H') {
     if (hidden) {
       hidden = false;
-      input.show();
+      input.style.visibility = "visible";
+      colorT.style.visibility = "visible";
+      colorB.style.visibility = "visible";
 
-      sliderR.show();
-      sliderG.show();
-      sliderB.show();
-
-      sliderRB.show();
-      sliderGB.show();
-      sliderBB.show();
     } else {
       hidden = true;
-      input.hide();
-
-      sliderR.hide();
-      sliderG.hide();
-      sliderB.hide();
-
-      sliderRB.hide();
-      sliderGB.hide();
-      sliderBB.hide();
+      input.style.visibility = "hidden";
+      colorT.style.visibility = "hidden";
+      colorB.style.visibility = "hidden";
     }
   }
 }
